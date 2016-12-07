c9024bb066eaa9c5b667c9f6bdacf166d78bf4cc
eros <cababletech@gmail.comm>
2016-12-07 18:46:17 +0530

    Add new class for logout

---
+++ cabable-inventory/src/main/java/com/apifest/client/OAuthTokenRevocationRequest.java
@@ -0,0 +1,26 @@
+package com.apifest.client;
+
+public class OAuthTokenRevocationRequest {
+
+	private String access_token;
+	
+	private String client_id;
+
+	public String getAccess_token() {
+		return access_token;
+	}
+
+	public void setAccess_token(String access_token) {
+		this.access_token = access_token;
+	}
+
+	public String getClient_id() {
+		return client_id;
+	}
+
+	public void setClient_id(String client_id) {
+		this.client_id = client_id;
+	}
+	
+	
+}
